72d1decd99f0b413105f7bffc4da4ad0c2f60107
Plamen Andonov <plamen.p.andonov@gmail.com>
2018-04-23 00:32:09 +0100

    main.js file to include all the modules

---
+++ main.js
@@ -0,0 +1,5 @@
+const validatePostCode = require('./validatePostCode');
+
+var window = {};
+window.components = {};
+window.components = Object.assign(window.components, {validatePostCode: validatePostCode});
